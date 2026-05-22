09d12b01ccebe22c8597ed8265e78d57a51a67dd
dg2026810-blip <dg2026810@danggok.hs.kr>
2026-05-22 09:11:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 Web VPython 3.2
-#춘식이의얼굴
-sphere(pos = vec(0,0,0) * 100, color = vec(203/255, 175/255,81/255), radius = 1 * 100)
-sphere(pos = vec(0.8,0.8,0) * 100, color = vec(203/255, 175/255,81/255), radius = 0.2 * 100)
-sphere(pos = vec(-0.8,0.8,0) * 100, color = vec(203/255, 175/255,81/255), radius = 0.2 * 100)
+SIZE = 300
 
-#춘식이의귀
-sphere(pos = vec(0.1,0.25,0.95) * 100, color = color.white, radius = 0.15 * 100)
-sphere(pos = vec(-0.1,0.25,0.95) * 100, color = color.white, radius = 0.15 * 100)
+# 춘식이의 얼굴
+sphere(pos = vec(0,0,0) * SIZE, color = vec(203/255, 175/255,81/255), radius = 1 * SIZE)
+sphere(pos = vec(0.8,0.8,0) * SIZE, color = vec(203/255, 175/255,81/255), radius = 0.2 * SIZE)
+sphere(pos = vec(-0.8,0.8,0) * SIZE, color = vec(203/255, 175/255,81/255), radius = 0.2 * SIZE)
 
-#춘식이의코
-sphere(pos = vec(0,0.4,1) * 100, color = color.black, radius = 0.05 * 100)
+# 춘식이의 귀
+sphere(pos = vec(0.1,0.25,0.95) * SIZE, color = color.white, radius = 0.15 * SIZE)
+sphere(pos = vec(-0.1,0.25,0.95) * SIZE, color = color.white, radius = 0.15 * SIZE)
 
-#춘식이의눈
-sphere(pos = vec(0.3,0.5,0.8) * 100, color = color.black, radius = 0.1 * 24)
-sphere(pos = vec(-0.3,0.5,0.8) * 100, color = color.black, radius = 0.1 * 24)
+# 춘식이의 코
+sphere(pos = vec(0,0.4,1) * SIZE, color = color.black, radius = 0.05 * SIZE)
 
-#춘식이의세포막
-sphere(pos = vec(0,0,0), color = color.green, radius = 50,opacity=0.5)
+# 춘식이의 눈
+sphere(pos = vec(0.3,0.5,0.8) * SIZE, color = color.black, radius = 0.1 * (SIZE * 0.24))
+sphere(pos = vec(-0.3,0.5,0.8) * SIZE, color = color.black, radius = 0.1 * (SIZE * 0.24))
 
-#DNA조각
-helix(pos=vec(0, 0, 0), axis=vec(20, 0, 0), color=color.red)
+# 춘식이의 세포막 (전체 크기에 맞춰 위치와 반지름 확대)
+sphere(pos = vec(0,0,0) * SIZE, color = color.green, radius = 0.3 * SIZE, opacity=0.5)
 
-#춘식이의작은세포
-sphere(pos=vector(-2,0,0),radius=1, color=color.red, opacity=0.5)
+# 춘식이의 작은세포 (전체 크기에 맞춰 위치와 반지름 확대)
+sphere(pos = vec(-2,0,0) * (SIZE/10), radius = 0.01 * SIZE, color=color.red, opacity=0.5)
 
-# 춘식이의말
-label(text='아자스',pos = vec(100,100,0))
+# 춘식이의 말 (글자 크기와 배치 위치 확대)
+label(text='아자스', pos = vec(1,1,0) * SIZE, height = 20)
